2d15c126a77fe29eee366c5b9700afd129eb17de
Pragyanshu <pragyanshu@prakya.com>
2024-10-10 20:19:55 +0530

    fixed help message typo

--- src/log/Message.java
+++ src/log/Message.java
@@ -6,13 +6,13 @@ public class Message {
             "\n"+
                     "Usage: look [OPTIONS] [FILE_PATH] [PATTERN] \n" +
                     "Search for Patterns in Files. \n"+
-                    "Example: look -w -i 'error' /path/to/logs\n" +
+                    "Example: look -w -i /path/to/logs error\n" +
                     "\n" +
                     "Options:\n" +
                     "  -w, -W          Search for patterns recursively in directories.\n" +
                     "  -i, -I          Perform case-insensitive pattern matching.\n" +
                     "  -c, -C          Will color code the output.\n" +
-                    "  -l, -L          Can specify the depth level for the seach by passing a number in the argumet\n\t\twith this flag set.\n" +
+                    "  -l, -L          Can specify the depth level for the search by passing a number in the argument\n\t\twith this flag set.\n" +
                     "  -r, -R          Will run a recursive search down from the give path, this Supersedes depth level flag.\n" +
                     "  -p, -P          Will log the performance metric for the search session.\n" +
                     "  -h, -H          Show this help message and exit.\n" +
